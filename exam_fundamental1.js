3adede466164a689b9f6fb966e2e19f6a21fab1c
setiawan <setiawanmukri@gmail.com>
2018-08-08 10:18:40 +0700

    exam_fundamental1.js
    ujian_fundamental1_MukriSetiawanBaguna

---
+++ exam_fundamental1.js
@@ -0,0 +1,49 @@
+//soal 1
+
+// var array = [5,-6,-8,4,-3,6];
+
+// var x = array.length;
+// var positif = 0;
+// var negatif = 0;
+
+// for (var i =0; i<x; i++)
+// {
+//     if(array[i]>0)
+//     {
+//         positif = positif + array[i];
+//     }
+//     else if (array[i]<0)
+//     {
+//         negatif += Math.abs(array[i]);
+//     }
+    
+// }
+
+// if (positif>negatif)
+// {
+//     console.log("positif wins");
+// }
+// else if(negatif> positif)
+// {
+//     console.log("negatif wins");
+// }
+// else
+// {
+//     console.log("DRAW")
+// }
+
+// console.log(positif);
+// console.log(negatif);
+
+
+//==================================
+//soal 2
+
+// var string = "A1%6&Z#7Cc9a";
+
+// var x = string.length;
+// var angka = string.replace(/[^0-9]/g, '').split('');
+// var huruf = string.replace(/[^a-z-A-Z]/g, '').split('');
+
+// console.log('angka = '+angka);
+// console.log('huruf = '+huruf);
